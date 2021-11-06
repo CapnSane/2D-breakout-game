1caae3e4be7082ef3051e656bcecb12619ad3016
Leonardo Schneider <lnrdschneider@gmail.com>
2021-11-06 10:58:33 -0300

    section 2 completed

--- js.js
+++ js.js
@@ -1,20 +1,25 @@
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = '#FF0000';
-ctx.fill();
-ctx.closePath();
+let x = 0;
+let y = canvas.height / 2;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = 'green';
-ctx.fill();
-ctx.closePath();
+let dx = 1;
+let dy = -0.45;
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
-ctx.stroke();
-ctx.closePath();
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.fillStyle = 'rgb(0, 149, 221)';
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 10);
