ab8f284e8122848ad972e3ca6a11eaffb9973d59
Leonardo Schneider <lnrdschneider@gmail.com>
2021-11-16 11:28:56 -0300

    section 9 completed

--- js.js
+++ js.js
@@ -36,8 +36,10 @@ for (let c = 0; c < brickColumnCount; c++) {
   }
 }
 
+// Listeners
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
+document.addEventListener('mousemove', mouseMoveHandler, false);
 
 function keyDownHandler(e) {
   if (e.key == 'Right' || e.key == 'ArrowRight') {
@@ -52,6 +54,13 @@ function keyUpHandler(e) {
     rightPressed = false;
   } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
     leftPressed = false;
+  }
+}
+
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
   }
 }
 
